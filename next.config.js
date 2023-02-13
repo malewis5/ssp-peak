a32625d47f3a0055c5b543efb559285a2d2fc6c7
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-02-13 12:55:10 -0700

    change home page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/table/people?page=0',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
